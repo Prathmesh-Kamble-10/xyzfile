6d3de5c7913315557d188a4f46fa2275fc1d71a1
Prathmesh-Kamble-10 <prathmeshkamble885@gmail.com>
2024-08-11 10:23:17 +0530

    python number code updated

---
+++ Number.py
@@ -0,0 +1,49 @@
+
+# ! Python Numbers
+# There are three numeric types in Python:
+
+# int
+# float
+# complex
+
+
+# x = 1000
+# y = 2.8
+# z = 1j
+
+# print(type(x), "\nvalue is = ", x)
+# print(type(y), "\nvalue is = ", y)
+# print(type(z), "\nvalue is = ", z)
+
+# ! Type conversion 
+
+# x = 1    # int
+# y = 2.8  # float
+# z = 1j   # complex
+
+#convert from int to float:
+# x = float(1)
+
+#convert from float to int:
+# y = int(2.8)
+
+#convert from int to complex:
+# z = complex(1)
+
+# print(x)
+# print(y)
+# print(z)
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+
+# ! random module 
+
+# import random
+
+# print(random.randrange(1,99))
+
+
+
