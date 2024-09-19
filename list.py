9d4b289c846385ed9bb88c39f0f1c10a71f54433
Prathmesh-Kamble-10 <prathmeshkamble885@gmail.com>
2024-09-19 23:13:55 +0530

    python list prg

---
+++ list.py
@@ -0,0 +1,37 @@
+# y = ["apple","mango","banana","shh","dfd","123",343,44,True,False]
+# print(y[:4])
+
+
+# loop
+# for i in y:
+#     print(i)
+
+
+# control statement
+# if "Shh" in y:
+#     print("shh present")
+# else:
+#     print("shh not present")
+
+# y[3]="kiwi"
+# y[4]="orange"
+# y[5:8] =["cherry","grapes","pineapple"]
+
+# insert() method
+# y.insert(8,"strawberry")
+# y.insert(10,"1")
+
+# append() method
+# y.append("2")
+# print(y)
+
+
+# extend()  method
+
+x = [1,2,4,5]
+z = [3,6,7,8]
+a = ("pk","mk")
+z.extend(a)
+x.extend(z)
+print(x)
+print(z)
