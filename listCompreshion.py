0457d1c843667dd3e654e6e6e46c9e590d22ba4d
Prathmesh-Kamble-10 <prathmeshkamble885@gmail.com>
2024-09-25 23:09:46 +0530

    list order list reverse list compreshion done

---
+++ listCompreshion.py
@@ -0,0 +1,19 @@
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = []
+
+# for x in fruits:
+#     if "a" in x:
+#         newlist.append(x)
+
+# print(newlist)
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = [y for y in fruits if "a" in y]
+# # newlist = [x for x in fruits if x != "apple"]
+# # newlist = [x for x in fruits]
+
+# print(newlist)
+
+fruits = ['apple', 'banana', 'cherry']
+newlist = [x for x in fruits]
+print(newlist)
---
+++ listsort.py
@@ -0,0 +1,26 @@
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# numberlist = [8,9,5,4,3,2]
+# # thislist.sort  #for ascending order
+# thislist.sort(reverse=True) #for decending order
+# # numberlist.sort() #for ascending order
+# numberlist.sort(reverse=True) # decending order
+# print(numberlist)
+# print(thislist)
+
+
+
+# sort why key str.lower method
+
+# thislist = ["BANANA", "ORANGE", "kiwi", "cherry"]
+
+# thislist.sort(key = str.lower)
+
+# print(thislist)
+
+# reverse method
+
+thislist = ["banana", "Orange", "Kiwi", "cherry"]
+
+thislist.reverse()
+
+print(thislist) 
