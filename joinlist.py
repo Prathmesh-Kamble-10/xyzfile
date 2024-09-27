64ecaf644798cf88387b5202c8de5c5d97047dae
Prathmesh-Kamble-10 <prathmeshkamble885@gmail.com>
2024-09-27 22:59:33 +0530

    Joinlist and copy list in list done

---
+++ joinlist.py
@@ -0,0 +1,25 @@
+# regular way/method
+
+# list1 = ["s","h","u"]
+# list2 = ["b","h","a","m"]
+# list3 = list1+list2
+# print(list3)
+
+# using method append()
+
+# list1 = ["s","h","u"]
+# list2 = ["b","h","a","m"]
+
+# for x in list2:
+#     list1.append(x)
+# print(list1)
+
+
+# using extend() method
+
+# list1 = ["s","h","u"]
+# list2 = ["b","h","a","m"]
+
+# list1.extend(list2)
+
+# print(list1)
---
+++ listcopy.py
@@ -0,0 +1,51 @@
+# # Depricated
+
+# thislist = ["apple","banana","cherry"]
+
+# y = thislist
+
+# y[2] = "mango"
+
+
+# print("This list data = ")
+# print(thislist)
+
+# print("Y list data = ")
+# print(y)
+
+
+# using copy method
+
+# thislist = ["apple", "banana", "cherry"]
+
+# cop = thislist.copy()
+
+# cop[2] = "mango"
+
+# print("This list data = ")
+# print(thislist)
+
+# print("cop list data = ")
+# print(cop)
+
+# Using list() method
+
+# thislist = ["apple", "banana", "cherry"]
+
+# mylist = list(thislist)
+
+# mylist[0] = "mango"
+
+# print(thislist)
+
+# print(mylist)
+
+
+# using slice operator : // list[:]  , tuple (:) , string [:]
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = thislist[:]
+
+# mylist[1] = "Orange"
+# print(thislist)
+# print(mylist)
