6fa859886cbff66a36c8a8d246772b084ce800b3
Prathmesh-Kamble-10 <prathmeshkamble885@gmail.com>
2024-11-03 22:37:00 +0530

    Tuple unpack and loop done

---
+++ Tuple/LoopTuple.py
@@ -0,0 +1,8 @@
+fruits = ("apple", "banana", "cherry")
+# print(fruits)
+# for x in fruits:
+#     print(x) 
+
+# for x in range(len(fruits)):
+#     print(fruits[x]) 
+
---
+++ Tuple/unpackTuple.py
@@ -0,0 +1,5 @@
+fruits=("apple","orange","kiwi","grasp","guava")
+(red,Orange,*green) = fruits
+print(Orange)
+print(red)
+print(green)
